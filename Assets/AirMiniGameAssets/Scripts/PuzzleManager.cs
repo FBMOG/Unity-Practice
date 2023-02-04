d8212119914e483d35b26b23efbc53f19b965484
SamuelMahadeo <77645938+SamuelMahadeo@users.noreply.github.com>
2023-02-04 17:56:50 -0400

    Drag and drop initial game finished
    simple drag and drop finished

--- Assets/AirMiniGameAssets/Scripts/PuzzleManager.cs
+++ Assets/AirMiniGameAssets/Scripts/PuzzleManager.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PuzzleManager : MonoBehaviour
 {
-
     [SerializeField] private List<PuzzleSlot> slotPrefabs;
     [SerializeField] private PuzzlePiece _piecePrefab;
     [SerializeField] private Transform slotParent, pieceParent;
+
+    private List<PuzzlePiece> spawnedPieces;
 
     void Start(){
         Spawn();
@@ -16,12 +18,25 @@ public class PuzzleManager : MonoBehaviour
 
     void Spawn() {
         var randomSet = slotPrefabs.OrderBy(s=>Random.value).Take(3).ToList();
+        spawnedPieces = new List<PuzzlePiece>();
 
         for(int i = 0; i < randomSet.Count; i++){
             var spawnedSlot = Instantiate(randomSet[i], slotParent.GetChild(i).position, Quaternion.identity);
         
             var spawnedPiece =  Instantiate(_piecePrefab, pieceParent.GetChild(i).position, Quaternion.identity);
             spawnedPiece.Init(spawnedSlot);
+            spawnedPieces.Add(spawnedPiece);
         }
+    }
+
+    void Update() {
+        if (spawnedPieces.All(p => p.IsInCorrectSlot)) {
+            ChangeScene();
+        }
+    }
+
+    void ChangeScene() {
+        
+        SceneManager.LoadScene("Start Menu");
     }
 }
--- Assets/AirMiniGameAssets/Scripts/PuzzlePiece.cs
+++ Assets/AirMiniGameAssets/Scripts/PuzzlePiece.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class PuzzlePiece : MonoBehaviour
 {
     private Vector3 screenPoint;
-    private Vector3 offset, originalPostion;
+    private Vector3 offset;
+    private Vector2 originalPostion;
     [SerializeField] private AudioSource _source;
     [SerializeField] private AudioClip _pickupClip, _dropClip;
     [SerializeField] private SpriteRenderer _renderer;
 
-    private PuzzleSlot slot;
-
+    private PuzzleSlot _currentSlot;
+    private bool placed;
+    
     public void Init(PuzzleSlot slot){
         _renderer.sprite = slot.Renderer.sprite;
-        this.slot = slot;
+        _currentSlot = slot;
     }
 
     void Awake() {
@@ -23,6 +25,9 @@ public class PuzzlePiece : MonoBehaviour
 
     void OnMouseDown()
     {
+        if (placed == true){
+            return;
+        }
         _source.PlayOneShot(_pickupClip);
         screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
         offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
@@ -31,11 +36,30 @@ public class PuzzlePiece : MonoBehaviour
     void OnMouseDrag()
     {
         Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        Vector2 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+        curPosition += (Vector2)offset;
         transform.position = curPosition;
     }
     
     void  OnMouseUp() {
-        transform.position = originalPostion;
+
+        if(Vector2.Distance(transform.position, _currentSlot.transform.position) < 3)
+        {
+            transform.position = _currentSlot.transform.position;
+            _currentSlot.Placed();
+            placed = true;
+        }
+        else
+        {
+            transform.position = originalPostion;
+        }
+            
+        
+    }
+
+    public bool IsInCorrectSlot {
+        get {
+            return _currentSlot != null && _currentSlot.transform.position == transform.position;
+        }
     }
 }
